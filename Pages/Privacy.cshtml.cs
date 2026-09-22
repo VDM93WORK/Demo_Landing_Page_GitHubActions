1fd74b6d6459e8165f7ee4795758114a5dbc44bb
Vincenzo Di Masi <vincenzo.dimasi@reactconsulting.it>
2026-09-22 15:11:32 +0200

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo_Landing_Page_GitHubActions.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
